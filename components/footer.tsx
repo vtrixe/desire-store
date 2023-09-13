a0f7f3586933d66f03d48670d6ca85e047ca96ec
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2023-09-14 02:45:23 +0530

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,19 @@
+
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
-      <div className="mx-auto py-10">
-        <p className="text-center text-xs text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
-        </p>
-      </div>
-    </footer>
+  <div className="mx-auto py-10">
+    <p className="text-center text-xs text-black">
+      &copy; Desire Supplements 103 Lajpat Nagar,Pakhowal Road, Ludhian(141001),Punjab,India
+      <br />
+      <a href="tel:+91 8196825557">+91 8196825557</a>
+      <br />
+      <a href="https://www.instagram.com/desire_supplement_/">@desire_supplement_</a>
+      <br />
+      <a href="mailto:desiresupplement.99@gmail.com">desire.supplement99@gmail.com</a>
+    </p>
+  </div>
+</footer>
   )
 };
 
--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -1,16 +1,21 @@
 import { Billboard } from "@/types";
 
 interface BillboardProps {
-  data: Billboard;
+  data: Billboard | null; // Ensure data can be null
 }
 
-const Billboard: React.FC<BillboardProps> = ({
-  data
-}) => {
-  console
-  return ( 
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  // Check if data is null or undefined
+  if (!data) {
+    return null; // Return null or some fallback content
+  }
+
+  return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+      <div
+        style={{ backgroundImage: `url(${data.imageUrl})` }}
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+      >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.label}
@@ -18,7 +23,7 @@ const Billboard: React.FC<BillboardProps> = ({
         </div>
       </div>
     </div>
-   );
+  );
 };
 
 export default Billboard;
