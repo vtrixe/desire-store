a220768abe0b3aafe71b4a6209cdfb7534cde1b0
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2023-10-12 16:10:10 +0530

    commit

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -29,7 +29,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
     <div className="bg-white">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
-          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 sm:grid-cols-2">
             <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               <Info data={product} />
